53f602ee8973e9c98be09e85670ffcee5f71da1c
neoming <liyiming1998@qq.com>
2023-10-09 23:04:56 +0800

    rustling day1

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,44 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut ans: String = String::new();
+    let mut head: bool = true;
+    for c in input.chars() {
+        if head && c != ' ' {
+            head = false;
+        }
+        if !head {
+            ans.push(c);
+        }
+    }
+    let mut final_ans: String = String::new();
+    head = true;
+    for c in ans.chars().rev() {
+        if head && c != ' ' {
+            head = false;
+        }
+        if !head {
+            final_ans.push(c);
+        }
+    }
+    let mut ans = String::new();
+    for c in final_ans.chars().rev() {
+        ans.push(c);
+    }
+    ans
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    String::from(input.to_owned() + " world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let ans: String = String::from(input);
+    ans.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -39,7 +62,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
